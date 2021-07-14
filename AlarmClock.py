1525edb7bc865fb3783598a8a4f85659d5e98025
spencerjamesdev <spencer.franke@gmail.com>
2021-07-14 11:42:51 -0500

    Fixed python interpreter error, now program runs

--- AlarmClock.py
+++ AlarmClock.py
@@ -6,16 +6,16 @@ class Alarm_clock_class:
         self.alarm_time = "7:00 am"
 
     def change_current_time(self):
-        self.current_time = input("Enter the current time:")
+        self.current_time = input("Enter the current time: ")
         print("Current time: ", self.current_time)
 
     def toggle_alarm(self):
-        user_input = input("Turn on alarm? Type 'yes' or 'no':")
+        user_input = input("Turn on alarm? Type 'yes' or 'no': ")
         if(user_input == "yes"):
             self.on_or_off = True
         elif(user_input == "no"):
             self.on_or_off = False
 
     def set_alarm_time(self):
-        self.alarm_time = input("Enter the time you would like to set your alarm to:")
+        self.alarm_time = input("Enter the time you would like to set your alarm to: ")
         print("Alarm time: ", self.alarm_time)
